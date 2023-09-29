69c9056e12888335a8516f2e313da3cf06852671
Admin <Admin@TEB123010>
2023-09-29 14:40:47 +0200

    Add project files.

---
+++ kap 2 uppgift 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lönen för person 1?");
+            string person1 = Console.ReadLine();
+            int per1 = int.Parse(person1);
+            Console.WriteLine("lönen för person 2?");
+            string person2 = Console.ReadLine();
+            int per2 = int.Parse(person2);
+            Console.WriteLine("lönen för person 3?");
+            string person3 = Console.ReadLine();
+            int per3 = int.Parse(person3);
+            int kostnaden = per1 + per2 + per3;
+            Console.WriteLine("kostnaden för alla 3 är " + kostnaden);
+            int medellönen = kostnaden / 3;
+            Console.WriteLine("medellönen är " + medellönen);
+        }
+    }
+}
+           
+            
